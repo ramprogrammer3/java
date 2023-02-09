6785a847394a970448b9f328cf5b6fed0320eddc
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 12:44:30 +0530

    operator-precedence

---
+++ Main6.java
@@ -0,0 +1,21 @@
+public class Main6 {
+    public static void main(String[] args) {
+        // System.out.println("Hello world ");
+        // int a = 6 * 5 - 34 / 2;
+        // int b = 6 / 5 - 34 * 2;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int x = 6;
+        // int y = 1;
+
+        // int k = x * y /2;
+
+        int a = 5;
+        int b = 1;
+        int c = 4;
+
+        int k = b *b - (4 * a * c )/ (2 * a);
+        System.out.println(k);
+    }
+}
