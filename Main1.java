9befd8a2c0d13caa5bdb24a9aad54875f9d6b925
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 07:49:21 +0530

    data-Tyep

---
+++ Main1.java
@@ -0,0 +1,39 @@
+public class Main1 {
+    public static void main(String[] args) {
+
+        // integer
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 40;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+        int age = 34;
+        byte age1 = 34;
+        System.out.println(age);
+        System.out.println(age1);
+
+        char ch = 'A';
+        float f1 =  343.4F;
+        double d1 = 43.343d;
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+
+        long agedyno = 435344343433l;
+        System.out.println(agedyno);
+
+        short number = 343;
+        System.out.println(number);
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String str = "ram kumar ";
+        System.out.println(str);
+
+    }
+}
