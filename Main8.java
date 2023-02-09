aa817aee92561962c41ce432dbaa41fa58ddc93d
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 13:35:20 +0530

    string-basic

---
+++ Main8.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        // System.out.println("Hello world ");
+        // float a = 7 /4.0f * 9 /2.0f;
+        // System.out.println(a);
+
+        // String str = "Hi my name is ram kumar ";
+        // System.out.println(str);
+
+        // String name = new String("Ram kumar ");
+        // System.out.println(name);
+
+        // String name1 = "maniyari";
+        // System.out.println(name1);
+        // name1 = "sitamahir";
+        // System.out.println(name1.length());
+        // System.out.println(name.concat(" sitamashiri"));
+
+        // int a = 6;
+        // float b = 34.435f;
+
+        // System.out.printf("The value of  a is %d %f ", a, b);
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        System.out.println(name);
+        sc.close();
+
+
+
+
+    }
+}
