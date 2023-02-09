c3db81da6d9ff116e83b75ec09550d6ab19d832c
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 13:16:00 +0530

    increment-decrement

---
+++ Main7.java
@@ -0,0 +1,42 @@
+public class Main7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        // int a = 654 + 6;
+        // float b = 435.3f + 3;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // byte x = 5;
+        // int y = 6;
+        // short z = 8;
+
+        // System.out.println(x + y);
+        // System.out.println(x + z);
+
+        // increment and decrement operator
+
+        int a = 10;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a);
+
+        int b = 340;
+        System.out.println(b--);
+        System.out.println(b);
+        System.out.println(--b);
+
+        int c = 7;
+        int ans = (++c) * (++c);
+        System.out.println(ans);
+
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x);
+
+        char ch = 'a';
+        System.out.println(++ch);
+
+    }
+}
