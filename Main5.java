5a21ba83ce3010a9c5a73653eabb2706e6e9d7d8
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 08:39:14 +0530

    oprator

---
+++ Main5.java
@@ -0,0 +1,48 @@
+public class Main5 {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+        int a = 5;
+        int b = 6 + a;
+        System.out.println(b);
+
+        b += 3;
+        System.out.println(b);
+        b -= 10;
+        System.out.println(b);
+        b *= 10;
+        System.out.println(b);
+
+        int c = 7;
+        System.out.println(b % c);
+        System.out.println(5 == 7);
+        // System.out.println(10 == 10);
+        System.out.println(6 > 4);
+        System.out.println(10 <= 134);
+        System.out.println(12 != 43);
+        System.out.println(34 != 'A');
+
+        System.out.println(343 > 5 && 34 > 8);
+
+        // System.out.println(34 > 10 || 34 < 'A');
+
+        System.out.println(34 >> 1);
+        System.out.println(34 >> 2);
+
+        System.out.println(34 << 1);
+        System.out.println(34 << 2);
+        System.out.println(40 & 1);
+
+        int c1 = 40 & 1;
+
+        // if(){
+        // System.out.println("odd");
+        // }else{
+        // System.out.println("Even");
+        // }
+
+        System.out.println(c1);
+
+        
+
+    }
+}
