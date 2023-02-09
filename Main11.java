fee341a5ee50441ad92afbd325461cc2a541424d
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-09 21:07:29 +0530

    if-else

---
+++ Main11.java
@@ -0,0 +1,15 @@
+public class Main11 {
+    public static void main(String[] args) {
+        // System.out.println("Hello world ");
+
+        int age = 6;
+        age = 34;
+
+        if (age > 18) {
+            System.out.println("You can drive ");
+        } else {
+            System.out.println("NO boy , You can not drive ");
+        }
+
+    }
+}
