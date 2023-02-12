01983198088dba264b24067c9d03f1925f8e5516
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-12 22:11:45 +0530

    logical-operator

---
+++ Main13.java
@@ -0,0 +1,23 @@
+
+public class Main13 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(true && false);
+        if(a && b){
+            System.out.println("Y");
+        }else{
+            System.out.println("N");
+        }
+
+        System.out.println(true && true);
+        System.out.println(false || true);
+
+        // System.out.println( true ! false);
+
+        
+
+    }
+}
