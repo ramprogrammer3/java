30b5abd3e0ec631f13e32124ed4f075772d5b519
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-13 07:11:29 +0530

    while-loop

---
+++ Main18.java
@@ -0,0 +1,21 @@
+public class Main18 {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        System.out.println("printing 1 2 3 using while loop ");
+        int i = 1;
+        while (i < 4) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        System.out.println("Finish running while loop ");
+
+        while (true) {
+            System.out.println("I am an infinite loop ");
+        }
+    }
+}
